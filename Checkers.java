cbbdb946841b552f9dce76f64b588d99d8ef1689
Dominykas <vais0001@hz.nl>
2024-01-18 20:24:32 +0100

    Spawned the players a and b on the board

--- Checkers.java
+++ Checkers.java
@@ -46,6 +46,7 @@ public class Checkers {
             }
         }
     }
+
     private static void printBoard() {
         int i;
         for(i = 1; i < 9; ++i) {
@@ -77,8 +78,133 @@ public class Checkers {
             System.out.print("+\n");
         }
     }
+
+    private static void processPlayerTurn(int player, Scanner in) {
+        boolean x = false;
+        boolean once = true;
+        int row1 = 0;
+        byte column1 = 0;
+
+        while(!x) {
+            ArrayList<Path> cellsGo = new ArrayList();
+            x = true;
+
+            for(int i = 0; i < 8; ++i) {
+                for(int j = 0; j < 8; ++j) {
+                    if (!once) {
+                        i = row1;
+                        j = column1;
+                    }
+
+                    Cell now = new Cell(i, j);
+                    if (player == 0 && board[i][j] == 'a') {
+                        if (i < 6 && j > 1 && (board[i + 1][j - 1] == 'b' || board[i + 1][j - 1] == 'B') && board[i + 2][j - 2] == ' ') {
+                            cellsGo.add(new Path(now, new Cell(i + 2, j - 2)));
+                            x = false;
+                        }
+
+                        if (i < 6 && j < 6 && (board[i + 1][j + 1] == 'b' || board[i + 1][j + 1] == 'B') && board[i + 2][j + 2] == ' ') {
+                            cellsGo.add(new Path(now, new Cell(i + 2, j + 2)));
+                            x = false;
+                        }
+                    } else if (player == 1 && board[i][j] == 'b') {
+                        if (i > 1 && j > 1 && (board[i - 1][j - 1] == 'a' || board[i - 1][j - 1] == 'A') && board[i - 2][j - 2] == ' ') {
+                            cellsGo.add(new Path(now, new Cell(i - 2, j - 2)));
+                            x = false;
+                        }
+
+                        if (i > 1 && j < 6 && (board[i - 1][j + 1] == 'a' || board[i - 1][j + 1] == 'A') && board[i - 2][j + 2] == ' ') {
+                            cellsGo.add(new Path(now, new Cell(i - 2, j + 2)));
+                            x = false;
+                        }
+                    } else if (player == 0 && board[i][j] == 'A') {
+                        if (i > 1 && j > 1 && (board[i - 1][j - 1] == 'b' || board[i - 1][j - 1] == 'B') && board[i - 2][j - 2] == ' ') {
+                            cellsGo.add(new Path(now, new Cell(i - 2, j - 2)));
+                            x = false;
+                        }
+
+                        if (i > 1 && j < 6 && (board[i - 1][j + 1] == 'b' || board[i - 1][j + 1] == 'B') && board[i - 2][j + 2] == ' ') {
+                            cellsGo.add(new Path(now, new Cell(i - 2, j + 2)));
+                            x = false;
+                        }
+
+                        if (i < 6 && j > 1 && (board[i + 1][j - 1] == 'b' || board[i + 1][j - 1] == 'B') && board[i + 2][j - 2] == ' ') {
+                            cellsGo.add(new Path(now, new Cell(i + 2, j - 2)));
+                            x = false;
+                        }
+
+                        if (i < 6 && j < 6 && (board[i + 1][j + 1] == 'b' || board[i + 1][j + 1] == 'B') && board[i + 2][j + 2] == ' ') {
+                            cellsGo.add(new Path(now, new Cell(i + 2, j + 2)));
+                            x = false;
+                        }
+                    } else if (player == 1 && board[i][j] == 'B') {
+                        if (i > 1 && j > 1 && (board[i - 1][j - 1] == 'a' || board[i - 1][j - 1] == 'A') && board[i - 2][j - 2] == ' ') {
+                            cellsGo.add(new Path(now, new Cell(i - 2, j - 2)));
+                            x = false;
+                        }
+
+                        if (i > 1 && j < 6 && (board[i - 1][j + 1] == 'a' || board[i - 1][j + 1] == 'A') && board[i - 2][j + 2] == ' ') {
+                            cellsGo.add(new Path(now, new Cell(i - 2, j + 2)));
+                            x = false;
+                        }
+
+                        if (i < 6 && j > 1 && (board[i + 1][j - 1] == 'a' || board[i + 1][j - 1] == 'A') && board[i + 2][j - 2] == ' ') {
+                            cellsGo.add(new Path(now, new Cell(i + 2, j - 2)));
+                            x = false;
+                        }
+
+                        if (i < 6 && j < 6 && (board[i + 1][j + 1] == 'a' || board[i + 1][j + 1] == 'A') && board[i + 2][j + 2] == ' ') {
+                            cellsGo.add(new Path(now, new Cell(i + 2, j + 2)));
+                            x = false;
+                        }
+                    }
+
+                    if (!once) {
+                        break;
+                    }
+                }
+
+                if (!once) {
+                    break;
+                }
+            }
+
+            if (x) {
+                break;
+            }
+
+            once = false;
+            printBoard();
+        }
+
+        if (once) {
+            printBoard();
+        }
+
+    }
+
     public static void main(String[] args) {
         initializeBoard();
         printBoard();
+    }
+}
+
+class Cell {
+    private int row;
+    private int col;
+
+    Cell(int r, int c) {
+        this.row = r;
+        this.col = c;
+    }
+}
+
+class Path {
+    private Cell start;
+    private Cell end;
+
+    Path(Cell s, Cell e) {
+        this.start = s;
+        this.end = e;
     }
 }
